77c5da4b25217ed37f4007d107a1580cdc56c152
AugustineKadima <kadimasam@gmail.com>
2022-07-14 11:05:10 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,41 @@
 class ItemsController < ApplicationController
 
+  # Get all
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+
+  rescue ActiveRecord::RecordNotFound
+    render json: {error: "Item not found"}, status: :not_found
+  end
+
+  # Find by id
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item, status: :ok, include: :user
+    else
+      render json: {error: "Item not found"}, status: :not_found
+    end
+  end
+
+  # Create
+
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = Item.create(params.permit(:name, :description, :price))
+      user.items << item
+      render json: item, status: 201
+    else
+      Item.create(params.permit(:name, :description, :price))
+    end
   end
 
 end
